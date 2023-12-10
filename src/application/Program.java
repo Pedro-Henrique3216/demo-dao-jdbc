08a8c9141769952089a86ba4fed700c471a3f40b
Pedro Henrique <ph6704938@gmail.com>
2023-12-10 17:15:03 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -8,6 +11,10 @@ public class Program {
 
         Department dp = new Department(1, "Books");
         System.out.println(dp);
+
+        Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.00, dp);
+
+        System.out.println(seller);
 
     }
 }
