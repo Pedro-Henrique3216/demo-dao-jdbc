bb03e11404a03d85320d33ed62d2b3161868b82a
Pedro Henrique <ph6704938@gmail.com>
2023-12-10 17:26:37 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
         System.out.println(dp);
 
         Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.00, dp);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
 
